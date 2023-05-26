28c6724d69ac3f13fac8ae9bf1665459b854e3b2
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-26 11:38:05 -0700

    docs: document scene.ts

--- src/World/components/scene.ts
+++ src/World/components/scene.ts
@@ -1,5 +1,14 @@
 import { Color, Scene } from 'three';
 
+/**
+ * Creates and returns a Three.js Scene object
+ *
+ * @date 5/26/2023 - 11:24:38 AM
+ * @author Ryan Milligan
+ *
+ * @export
+ * @returns {Scene} the Three.js Scene object
+ */
 export default function createScene() {
   const scene = new Scene();
   scene.background = new Color(0x2d3047); // color: 'Space Cadet'
