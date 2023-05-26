4f78af107070c41f557e3bdba99b63d73880ac72
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-25 18:46:58 -0700

    fix: fix prettier / eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,45 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'eslint-config-airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-typescript/base',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-typescript/base',
-        'prettier',
-    ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json'
-    },
-    plugins: ['@typescript-eslint', 'prettier'],
-    settings: {
-        'import/resolver': {
-            node: {
-                paths: ['src'],
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    },
-    rules: {
-        'prettier/prettier': 2, // Means error
-        'import/extensions': 0,
-        'import/no-extraneous-dependencies': 0
-    },
+  },
+  rules: {
+    indent: 0,
+    //'prettier/prettier': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/named': 0,
+    'import/namespace': 0,
+    'import/default': 0,
+    'import/no-named-as-default-member': 0,
+    'no-tabs': ['error', { allowIndentationTabs: true }],
+    '@typescript-eslint/indent': 0,
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { WebGLRenderer } from 'three';
 import './style.css';
-//import typescriptLogo from '/typescript.svg';
+
+/** Todo:
+ * Create Debug UI
+ * make debug UI optional via query parameters
+ */
 
 const main = () => {
-    // get reference to #app div element
-    //const app = document.querySelector<HTMLDivElement>('#app');
+  // get reference to #app div element
+  const app = document.querySelector<HTMLDivElement>('#app');
+  const canvas = document.createElement('canvas');
+  canvas.id = 'canvas';
+  const renderer = new WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  app?.appendChild(renderer.domElement);
+  // const controls = new OrbitControls();
 };
 
 window.addEventListener('load', main);
