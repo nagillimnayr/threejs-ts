ece85fe2727d1c1557c8eef48006e0213974d58e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-25 17:00:57 -0700

    build: install prettier

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import './style.css';
 //import typescriptLogo from '/typescript.svg';
 
 const main = () => {
-  // get reference to #app div element
-  const app = document.querySelector<HTMLDivElement>('#app');
-
-
-}
+    // get reference to #app div element
+    const app = document.querySelector<HTMLDivElement>('#app');
+};
 
 window.addEventListener('load', main);
