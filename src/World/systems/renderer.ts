ee0a33e78e5b6b6df6e1d4df6d2e123b1f42e0e3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-26 11:38:19 -0700

    docs: document renderer.ts

--- src/World/systems/renderer.ts
+++ src/World/systems/renderer.ts
@@ -1,5 +1,14 @@
 import { WebGLRenderer } from 'three';
 
+/**
+ * Creates and returns a Three.js WebGLRenderer object
+ *
+ * @date 5/26/2023 - 11:34:01 AM
+ * @author Ryan Milligan
+ *
+ * @export
+ * @returns {WebGLRenderer}
+ */
 export default function createRenderer() {
   const renderer = new WebGLRenderer({
     antialias: true,
